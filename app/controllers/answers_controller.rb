beab690b7e71898776c4c2d545d00d3c90a2a2c3
Artem Nikolenko <kopo4e.web@gmail.com>
2021-03-09 23:14:48 +0300

    Add: acceptance test - user may write an answer of question

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -14,9 +14,9 @@ class AnswersController < ApplicationController
   def create
     @answer = @question.answers.new(answer_params)
     if @answer.save
-      redirect_to @question
+      redirect_to @question, notice: 'Add new answer'
     else
-      render :new
+      render 'questions/show'
     end
   end
 
---
+++ spec/features/answer/create_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+feature 'User can create answer of question', %q{
+  In order to help for a community
+  As an any user
+  I'd like to be able to given on the questions
+} do
+
+  given(:question) { create(:question) }
+
+  scenario 'user gives on answer on a question' do
+    visit question_path(question)
+
+    expect(page).to have_content 'New answer'
+    fill_in 'Body', with: 'New answer'
+    click_on 'Give an answer'
+
+    expect(page).to have_content 'Add new answer'
+  end
+
+  scenario 'user gives on answer on a question with errors' do
+    visit question_path(question)
+
+    expect(page).to have_content 'New answer'
+    click_on 'Give an answer'
+
+    expect(page).to have_content "Body can't be blank"
+  end
+
+end
