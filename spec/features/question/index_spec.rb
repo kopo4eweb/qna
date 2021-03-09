6d604a9cdd9ffb65bbac6a2e6d07592815e93b26
Artem Nikolenko <kopo4e.web@gmail.com>
2021-03-09 22:12:11 +0300

    Add: acceptance test - user may look all question

---
+++ spec/features/question/index_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+feature 'User can view all question', %q{
+  In order to get answer from a community
+  As an any user
+  I'd like to be able to look the all questions
+} do
+
+  scenario 'the all questions' do
+    questions = create_list(:question, 2)
+
+    visit questions_path
+
+    expect(page).to have_content 'List question'
+
+    expect(page).to have_content questions[0].title
+    expect(page).to have_content questions[0].body
+
+    expect(page).to have_content questions[1].title
+    expect(page).to have_content questions[1].body
+  end
+end
