57c5f9454bd63946e0856a0892bf005a481ae561
Artem Nikolenko <kopo4e.web@gmail.com>
2021-03-11 22:30:20 +0300

    Upd: tests - answers

--- spec/features/answer/create_spec.rb
+++ spec/features/answer/create_spec.rb
@@ -4,27 +4,47 @@ require 'rails_helper'
 
 feature 'User can create answer of question', %q(
   In order to help for a community
-  As an any user
+  As an authenticated user
   I'd like to be able to given on the questions
 ) do
+  given(:user) { create(:user) }
   given(:question) { create(:question) }
 
-  scenario 'user gives on answer on a question' do
+  background do
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Log in'
+  end
+
+  scenario 'Authenticated user gives on answer on a question' do
     visit question_path(question)
 
     expect(page).to have_content 'New answer'
-    fill_in 'Body', with: 'New answer'
+    fill_in 'Body', with: 'New answer for question'
     click_on 'Give an answer'
 
     expect(page).to have_content 'Add new answer'
   end
 
-  scenario 'user gives on answer on a question with errors' do
+  scenario 'Authenticated user gives on answer on a question with errors' do
     visit question_path(question)
 
     expect(page).to have_content 'New answer'
     click_on 'Give an answer'
 
     expect(page).to have_content "Body can't be blank"
+  end
+
+  scenario 'Unauthenticated user tries gives on answer on a question' do
+    click_on 'Sign out'
+
+    visit question_path(question)
+
+    expect(page).to have_content 'New answer'
+    fill_in 'Body', with: 'New answer for question'
+    click_on 'Give an answer'
+
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
   end
 end
