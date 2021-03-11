db379e5083a069f248464bbaf5d4660d6a11f2b4
Artem Nikolenko <kopo4e.web@gmail.com>
2021-03-11 19:26:55 +0300

    Add: gem devise, model User

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
+
   resources :questions do
     resource :answers, only: :create
   end
+
+  root to: 'questions#index'
 end
