3ddb5fbb880f83bd774233da6d6822c191f04610
Artem Nikolenko <kopo4e.web@gmail.com>
2021-03-11 14:00:55 +0300

    Add: acceptance test - user may look question and answers by it

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -5,32 +5,6 @@ require 'rails_helper'
 RSpec.describe AnswersController, type: :controller do
   let(:question) { create(:question) }
 
-  describe 'GET #show' do
-    let(:answer) { create(:answer, question: question) }
-
-    before { get :show, params: { id: answer } }
-
-    it 'assigns the requested answer to @answer' do
-      expect(assigns(:answer)).to eq answer
-    end
-
-    it 'renders show view' do
-      expect(response).to render_template :show
-    end
-  end
-
-  describe 'GET #new' do
-    before { get :new, params: { question_id: question } }
-
-    it 'assigns a new Answer to @answer' do
-      expect(assigns(:answer)).to be_a_new(Answer)
-    end
-
-    it 'renders new view' do
-      expect(response).to render_template :new
-    end
-  end
-
   describe 'POST #create' do
     context 'with valid attributes' do
       it 'saves a new answer for the question in the database' do
---
+++ spec/features/question/show_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature 'User can to look a question and answers for it', %q{
+  In order to get answer from a community
+  As an any user
+  I'd like to be able to look the question and answers for it
+} do
+
+  given(:question) { create(:question) }
+
+  scenario 'show question and answers' do
+    create(:answer, question: question)
+
+    visit question_path(question)
+
+    expect(page).to have_content 'MyString'
+    expect(page).to have_content 'MyText'
+
+    expect(page).to have_content 'MyText Answer'
+  end
+end
