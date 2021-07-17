1153a000b792dca8a49c7c13b3f2799807f27fed
Artem Nikolenko <kopo4e.web@gmail.com>
2021-07-17 15:53:11 +0300

    Add: bootstrap, jquery, styling template

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,15 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+require("jquery")
+require("bootstrap")
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import $ from 'jquery'
+window.jQuery = $;
+window.$ = $;
+
+import "../stylesheets/all.sass"
