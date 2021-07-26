5e1d7de742cf8a9109bba9ed7b39becfcd72a385
Artem Nikolenko <kopo4e.web@gmail.com>
2021-07-26 22:36:10 +0300

    Add: single link to a question or an answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,7 +4,11 @@ class Answer < ApplicationRecord
   belongs_to :user
   belongs_to :question
 
+  has_many :links, dependent: :destroy, as: :linkable
+
   has_many_attached :files
+
+  accepts_nested_attributes_for :links, reject_if: :all_blank, allow_destroy: true
 
   validates :body, presence: true
 
