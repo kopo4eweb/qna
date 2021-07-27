87aa36cb11d23bf936ed309c53ed6a613aed33aa
Artem Nikolenko <kopo4e.web@gmail.com>
2021-07-27 23:34:47 +0300

    Add: validate url format

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,4 +4,5 @@ class Link < ApplicationRecord
   belongs_to :linkable, polymorphic: true
 
   validates :name, :url, presence: true
+  validates :url, url: true
 end
--- spec/features/question/add_links_spec.rb
+++ spec/features/question/add_links_spec.rb
@@ -9,8 +9,9 @@ feature 'User can add links to question', %q(
 ) do
   given(:user) { create(:user) }
   given(:gist_url) { 'https://gist.github.com/kopo4eweb/ee186726f9a58f3f778888117d9f3701' }
+  given(:bad_url) { 'bad_url.org' }
 
-  scenario 'User adds link when asks question' do
+  background do
     sign_in(user)
     visit new_question_path
 
@@ -18,10 +19,22 @@ feature 'User can add links to question', %q(
     fill_in 'Body', with: 'text text text'
 
     fill_in 'Link name', with: 'My gist'
+  end
+
+  scenario 'User adds link when asks question' do
     fill_in 'Url', with: gist_url
 
     click_on 'Ask'
 
     expect(page).to have_link 'My gist', href: gist_url
+  end
+
+  scenario 'User adds link when asks question with error' do
+    fill_in 'Url', with: bad_url
+
+    click_on 'Ask'
+
+    expect(page).to have_content 'Links url is not a valid URL'
+    expect(page).not_to have_link 'My gist', href: bad_url
   end
 end
