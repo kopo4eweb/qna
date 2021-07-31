23425eaf0420124ff7998218b608f42979edb1cd
Artem Nikolenko <kopo4e.web@gmail.com>
2021-07-31 21:40:14 +0300

    Add: edit and remove links

--- spec/features/question/edit_spec.rb
+++ spec/features/question/edit_spec.rb
@@ -9,6 +9,7 @@ feature 'User can edit his question', %q(
 ) do
   given(:user) { create(:user) }
   given!(:question) { create(:question) }
+  given(:google_url) { 'https://google.com' }
 
   scenario 'Unauthenticated can not edit answer' do
     visit questions_path
@@ -45,6 +46,52 @@ feature 'User can edit his question', %q(
 
       expect(page).to have_link 'rails_helper.rb'
       expect(page).to have_link 'spec_helper.rb'
+    end
+
+    scenario 'can add link while edit his question' do
+      sign_in(question.user)
+      visit questions_path
+
+      click_on 'Edit question'
+
+      click_on 'Add link'
+
+      within('.questions') do
+        fill_in 'Link name', with: 'My google'
+        fill_in 'Url', with: google_url
+        click_on 'Save'
+      end
+
+      visit question_path(question)
+      expect(page).to have_link 'My google', href: google_url
+    end
+
+    scenario 'can remove link while edit his question' do
+      sign_in(question.user)
+      visit questions_path
+      click_on 'Edit question'
+
+      click_on 'Add link'
+
+      within('.questions') do
+        fill_in 'Link name', with: 'My google'
+        fill_in 'Url', with: google_url
+        click_on 'Save'
+      end
+
+      visit question_path(question)
+      expect(page).to have_link 'My google', href: google_url
+
+      visit questions_path
+      click_on 'Edit question'
+
+      within('.questions') do
+        click_on 'Remove link'
+        click_on 'Save'
+      end
+
+      visit question_path(question)
+      expect(page).not_to have_link 'My google'
     end
 
     scenario 'open and cancel edit his question' do
