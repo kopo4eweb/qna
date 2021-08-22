ad5af0410567a5a2ea1b55e730e4aad817580585
Artem Nikolenko <kopo4e.web@gmail.com>
2021-08-22 15:03:37 +0300

    Upd: refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,19 +23,6 @@ class User < ApplicationRecord
   end
 
   def self.find_for_oauth(auth)
-    authorization = Authorization.find_by(provider: auth.provider, uid: auth.uid.to_s)
-    return authorization.user if authorization
-
-    email = auth.info[:email]
-    user = User.find_by(email: email)
-
-    unless user
-      password = Devise.friendly_token[0, 20]
-      user = User.create!(email: email, password: password, password_confirmation: password)
-    end
-
-    user.authorizations.create(provider: auth.provider, uid: auth.uid.to_s)
-
-    user
+    FindForOauthService.new(auth).call
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -46,69 +46,20 @@ RSpec.describe User, type: :model do
     end
   end
 
+  # rubocop:disable RSpec/MessageSpies
+  # rubocop:disable RSpec/VerifiedDoubles
+  # rubocop:disable RSpec/StubbedMock
   describe '.find_for_oauth' do
-    let!(:user) { create(:user) }
     let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456') }
+    let(:service) { double('FindForOauthService') }
 
-    context 'when user already has authorization' do
-      it 'return the user' do
-        user.authorizations.create(provider: 'facebook', uid: '123456')
-        expect(described_class.find_for_oauth(auth)).to eq user
-      end
-    end
-
-    context 'when user has not authorization' do
-      context 'when user already exists' do
-        let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456', info: { email: user.email }) }
-
-        it 'does not create new  user' do
-          expect { described_class.find_for_oauth(auth) }.not_to change(described_class, :count)
-        end
-
-        it 'creates authorization for user' do
-          expect { described_class.find_for_oauth(auth) }.to change(user.authorizations, :count).by(1)
-        end
-
-        it 'creates authorization with provider ans uid' do
-          authorization = described_class.find_for_oauth(auth).authorizations.first
-
-          expect(authorization.provider).to eq auth.provider
-          expect(authorization.uid).to eq auth.uid
-        end
-
-        it 'returns the user' do
-          expect(described_class.find_for_oauth(auth)).to eq user
-        end
-      end
-    end
-
-    context 'when user does not exist' do
-      let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456', info: { email: 'new@user.com' }) }
-
-      it 'creates new user' do
-        expect { described_class.find_for_oauth(auth) }.to change(described_class, :count).by(1)
-      end
-
-      it 'returns new user' do
-        expect(described_class.find_for_oauth(auth)).to be_a(described_class)
-      end
-
-      it 'fills user email' do
-        user = described_class.find_for_oauth(auth)
-        expect(user.email).to eq auth.info.email
-      end
-
-      it 'creates authorization for user' do
-        user = described_class.find_for_oauth(auth)
-        expect(user.authorizations).not_to be_empty
-      end
-
-      it 'creates authorization for user with provider and uid' do
-        authorization = described_class.find_for_oauth(auth).authorizations.first
-
-        expect(authorization.provider).to eq auth.provider
-        expect(authorization.uid).to eq auth.uid
-      end
+    it 'calls FindForOauthService' do
+      expect(FindForOauthService).to receive(:new).with(auth).and_return(service)
+      expect(service).to receive(:call)
+      described_class.find_for_oauth(auth)
     end
   end
+  # rubocop:enable RSpec/MessageSpies
+  # rubocop:enable RSpec/VerifiedDoubles
+  # rubocop:enable RSpec/StubbedMock
 end
