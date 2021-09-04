82ff7cb805ae19125d752a782edc969c09b1091f
Artem Nikolenko <kopo4e.web@gmail.com>
2021-09-04 22:38:44 +0300

    Add: refactoring shared

--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -6,16 +6,10 @@ describe 'Questions API', type: :request do
   let(:headers) { { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' } }
 
   describe 'GET /api/v1/questions' do
-    context 'when unauthorized' do
-      it 'returns 401 status if there is not access_token' do
-        get '/api/v1/questions', headers: headers
-        expect(response.status).to eq 401
-      end
+    let(:api_path) { '/api/v1/questions' }
 
-      it 'returns 401 status if access_token is invalid' do
-        get '/api/v1/questions', params: { access_token: '12345' }, headers: headers
-        expect(response.status).to eq 401
-      end
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :get }
     end
 
     # rubocop:disable RSpec/MultipleMemoizedHelpers
@@ -26,7 +20,7 @@ describe 'Questions API', type: :request do
       let(:question_json) { json['questions'].first }
       let!(:answers) { create_list(:answer, 3, question: question) }
 
-      before { get '/api/v1/questions', params: { access_token: access_token.token }, headers: headers }
+      before { get api_path, params: { access_token: access_token.token }, headers: headers }
 
       it 'returns 200 status' do
         expect(response).to be_successful
---
+++ spec/support/shared/api_authorization.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+shared_examples_for 'API Authorizable' do
+  context 'when unauthorized' do
+    it 'returns 401 status if there is not access_token' do
+      do_request(method, api_path, headers: headers)
+      expect(response.status).to eq 401
+    end
+
+    it 'returns 401 status if access_token is invalid' do
+      do_request(method, api_path, params: { access_token: '12345' }, headers: headers)
+      expect(response.status).to eq 401
+    end
+  end
+end
