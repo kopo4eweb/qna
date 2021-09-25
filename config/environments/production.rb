10f5af3aa42e8999742a681b7598ff2f33ddf1c8
Artem Nikolenko <kopo4e.web@gmail.com>
2021-09-25 23:03:13 +0300

    Add: yandex send mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,19 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "qna_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'smtp.yandex.ru', from: Rails.application.credentials[Rails.env.to_sym][:yandex][:user] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    :tls                   =>  true,
+    :address               => Rails.application.credentials[Rails.env.to_sym][:yandex][:user],
+    :port                  => 465,
+    :authentication        => :plain,
+    :user_name             => Rails.application.credentials[Rails.env.to_sym][:yandex][:user],
+    :password              => Rails.application.credentials[Rails.env.to_sym][:yandex][:password],
+    :enable_starttls_auto  => true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
